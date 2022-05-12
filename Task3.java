9af08ba6add515398b797724033de5b4be4d7417
YoussofAttallah <Youssof.Attallah@tkh.edu.eg>
2022-05-12 22:02:53 +0200

    Corrected naming errors

--- Task3.java
+++ Task3.java
@@ -1,94 +1,57 @@
 public class Task3 {
-    public static void main(String[] arg) {
-        System.out.println("The Min Heap is ");
-        MinHeap minHeap = new MinHeap(15);
-        minHeap.insert(5);
-        minHeap.insert(3);
-        minHeap.insert(17);
-        minHeap.insert(10);
-        minHeap.insert(84);
-        minHeap.insert(19);
-        minHeap.insert(6);
-        minHeap.insert(22);
-        minHeap.insert(9);
-        minHeap.print();
-        System.out.println("The Min val is "
-                + minHeap.remove());
+    public static void main(String[] args) {
+        int a[] = { 90, 87, 65, 11, 23, 45, 21 };
+        HeapSortClass h = new HeapSortClass(a, a.length);
+        h.heapSort();
+        ;
     }
 }
 
-class MinHeap {
-    private int[] Heap;
-    private int size;
-    private int maxsize;
-    private static final int FRONT = 1;
-    public MinHeap(int maxsize) {
-        this.maxsize = maxsize;
-        this.size = 0;
-        Heap = new int[this.maxsize + 1];
-        Heap[0] = Integer.MIN_VALUE;
+class HeapSortClass {
+    int array[];
+    int array_length;
+
+    public HeapSortClass(int a[], int al) {
+        this.array = a;
+        this.array_length = al;
     }
-    private boolean isLeaf(int pos) {
-        if (pos > (size / 2) && pos <= size) {
-            return true;
-        
-        }
-        return false;
-    }
-    private void swap(int fpos, int spos) {
-        int tmp;
-        tmp = Heap[fpos];
-        Heap[fpos] = Heap[spos];
-        Heap[spos] = tmp;
-    }
-    private void minHeapify(int pos) {
-        if (!isLeaf(pos)) {
-            if (Heap[pos] > Heap[2 * (pos)]
-                    || Heap[pos] > Heap[(2 * pos) + 1]) {
-                if (Heap[2 * (pos)] < Heap[(2 * pos) + 1]) {
-                    swap(pos, 2 * (pos));
-                    minHeapify(2 * pos);
-                }
-                else {
-                    swap(pos, (2 * pos) + 1);
-                    minHeapify((2 * pos) + 1);
-                }
-            }
-        }
-    }
-    public void insert(int element) {
-        if (size >= maxsize) {
-            return;
-        }
-        Heap[++size] = element;
-        int current = size;
-        while (Heap[current] < Heap[(current) / 2]) {
-            swap(current, current / 2);
-            current = current / 2;
+
+    public void heapify(int size, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1; // left child
+        int right = 2 * i + 2; // right child
+        // If left child is larger than root
+        if (left < size && array[left] > array[largest])
+            largest = left;
+        // If right child is larger than root
+        if (right < size && array[right] > array[largest])
+            largest = right;
+        // If root is not largest
+        if (largest != i) {
+            // swap a[i] with a[largest]
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+            heapify(size, largest);
         }
     }
 
-    // Method 8
-    // To print the contents of the heap
-    public void print() {
-        for (int i = 1; i <= size / 2; i++) {
-            // Printing the parent and both childrens
-            System.out.print(
-                    " PARENT : " + Heap[i]
-                            + " LEFT CHILD : " + Heap[2 * i]
-                            + " RIGHT CHILD :" + Heap[2 * i + 1]);
-            // By here new line is required
-            System.out.println();
+    public void heapSort() {
+        for (int i = array_length / 2 - 1; i >= 0; i--) {
+            heapify(array_length, i);
         }
+        for (int i = array_length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(i, 0);
+        }
+        printSorted();
     }
 
-    // Method 9
-    // To remove and return the minimum
-    // element from the heap
-    public int remove() {
-        int popped = Heap[FRONT];
-        Heap[FRONT] = Heap[size--];
-        minHeapify(FRONT);
-        return popped;
+    public void printSorted() {
+        for (int i = 0; i < array_length; i++) {
+            System.out.print(array[i] + ",");
+        }
     }
 }
